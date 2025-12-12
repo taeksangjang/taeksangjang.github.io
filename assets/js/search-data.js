a640bec7ea3cf03c97e2c3dbe1cb855489c7b593
taeksangjang <tsjang95@gmail.com>
2025-12-12 11:28:18 +0000

    Deploying to gh-pages from @ taeksangjang/taeksangjang.github.io@dccb748ba7a81956eed9270bac8f92e366e18b5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,8 +414,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-",
+          title: '',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
