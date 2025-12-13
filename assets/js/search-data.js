8859a089da478e8772552976a83c84013d05e569
taeksangjang <tsjang95@gmail.com>
2025-12-13 18:44:14 +0000

    Deploying to gh-pages from @ taeksangjang/taeksangjang.github.io@4d71e06bb2b7df9b562856c6b9d08731cad51f20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -87,7 +87,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/mycv.pdf", "_blank");
         },
       },{
         id: 'social-email',
